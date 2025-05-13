13efaa226d537a332b285171028edc5e0a39d9f2
Eric Bonelli leonel <ericbonelli@yahoo.com.br>
2025-05-13 17:19:34 -0400

    Update Profissão Cientista de Dados M31 - app_RFV.py

--- Profissão Cientista de Dados M31 - app_RFV.py
+++ Profissão Cientista de Dados M31 - app_RFV.py
@@ -10,12 +10,12 @@ from datetime            import datetime
 from PIL                 import Image
 from io                  import BytesIO
 
-@st.cache
+@st.cache_data_data
 def convert_df(df):
     return df.to_csv(index=False).encode('utf-8')
 
 # Função para converter o df para excel
-@st.cache
+@st.cache_data_data
 def to_excel(df):
     output = BytesIO()
     writer = pd.ExcelWriter(output, engine='xlsxwriter')
